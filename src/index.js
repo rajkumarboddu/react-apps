afea4e334ead394731d1966d8bbc2fadb63871b3
rajkumarboddu <rajkumar.boddu1311@gmail.com>
2018-08-14 17:32:58 +0530

    Add Login Component
    Basic layout created for login component.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles.css";
+
+import Login from "./login.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/login.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row align-items-center justify-content-center">
+          <h1>React Quiz</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
